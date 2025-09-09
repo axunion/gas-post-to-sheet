85283f070dce09379b12aab83ccd05f85605d751
axunion <120321953+axunion@users.noreply.github.com>
2025-09-09 23:13:58 +0900

    Implement doGet function with response handling for success and error cases

---
+++ src/doGet.ts
@@ -0,0 +1,51 @@
+type GetSuccessResponse = {
+	result: "done";
+	expired: boolean;
+};
+
+type GetErrorResponse = {
+	result: "error";
+	error: string;
+};
+
+type GetResponse = GetSuccessResponse | GetErrorResponse;
+
+const COL_EXPIRED = 0;
+
+function _doGet() {
+	const e = { parameter: { type: "" } };
+	const result = doGet(e as unknown as GoogleAppsScript.Events.DoGet);
+	console.log(result.getContent());
+}
+
+function doGet(
+	e: GoogleAppsScript.Events.DoGet,
+): GoogleAppsScript.Content.TextOutput {
+	let response: GetResponse;
+
+	try {
+		const type = e.parameter.type;
+
+		if (!type) {
+			throw new Error("Invalid parameter.");
+		}
+
+		const properties = PropertiesService.getScriptProperties().getProperties();
+		const configSheetId = properties.SPREADSHEET_ID_CONFIG;
+
+		if (!configSheetId) {
+			throw new Error("Invalid script properties.");
+		}
+
+		const config = getConfigRow(configSheetId, type);
+
+		response = {
+			result: "done",
+			expired: config[COL_EXPIRED] === true,
+		};
+	} catch (error) {
+		response = { result: "error", error: error.message };
+	}
+
+	return ContentService.createTextOutput(JSON.stringify(response));
+}
